5ff103b1f8da6cc080c74ee78d31da9d8b0c1bf8
Felipe Anargyrou <anargyrou4@hotmail.com>
2021-01-24 07:16:38 +0000

    Fixed Inventories not being properly reset after GameMode ends

--- Assets/Scripts/GameMode.cs
+++ Assets/Scripts/GameMode.cs
@@ -6,17 +6,26 @@ public class GameMode : MonoBehaviour
 {
     public Inventory playerInventory;
     public Inventory toolboxInventory;
+    public Item[] items;
     BreakableComponent[] shipComponents;
 
     // Start is called before the first frame update
     void Start()
     {
         shipComponents = FindObjectsOfType<BreakableComponent>();
-        
+
+        playerInventory.items.Clear();
+        toolboxInventory.items.Clear();
+
         if (shipComponents != null && shipComponents.Length > 0)
         {
             int i = Random.Range(0, shipComponents.Length - 1);
             shipComponents[i].ActivateEvent();
+        }
+
+        foreach (Item item in items)
+        {
+            toolboxInventory.Add(item);
         }
     }
 
